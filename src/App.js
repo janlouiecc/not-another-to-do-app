85c17fa8d282542882b9a00b4c11b934dca40845
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2023-02-02 10:05:09 -0700

    added complete task functionality

--- src/App.js
+++ src/App.js
@@ -14,12 +14,25 @@ const App = () => {
     const task = {
       id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
       taskName: newTask,
+      completed: false,
     };
     setTasks([...tasks, task]);
   };
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
   };
 
   return (
@@ -33,9 +46,11 @@ const App = () => {
         {tasks.map((task) => {
           return (
             <Task
+              completeTask={completeTask}
               deleteTask={deleteTask}
               taskName={task.taskName}
               id={task.id}
+              completed={task.completed}
             />
           );
         })}
--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,10 @@
-const Task = ({ taskName, id, deleteTask }) => {
+const Task = ({ taskName, id, completed, completeTask, deleteTask }) => {
   return (
     <div>
-      <h2>{taskName}</h2>
+      <h2 style={{ textDecorationLine: completed ? 'line-through' : 'none' }}>
+        {taskName}
+      </h2>
+      <button onClick={() => completeTask(id)}>Complete</button>
       <button onClick={() => deleteTask(id)}>x</button>
     </div>
   );
