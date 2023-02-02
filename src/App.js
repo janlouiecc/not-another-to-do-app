562d08fdbc230df421a6be041addc738948eaa6b
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2023-02-02 09:31:15 -0700

    basic functionality completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (task) => {
+    setTasks(
+      tasks.filter((t) => {
+        return t !== task;
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='addTask'>
+        <input type='text' value={newTask} onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+
+      <div className='list'>
+        {tasks.map((task) => {
+          return (
+            <div>
+              <h2>{task}</h2>
+              <button onClick={() => deleteTask(task)}>x</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
